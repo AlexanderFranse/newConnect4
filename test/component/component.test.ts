88774820b1cc11327c5200d23b73ce8db27b8b4c
mathijs.coenen@ns.nl <mathijs.coenen@ns.nl>
2024-09-04 09:35:45 +0200

    fix: eslint

--- test/component/component.test.ts
+++ test/component/component.test.ts
@@ -8,12 +8,12 @@ describe("It should be possible to start a new game", () => {
     expect(newGame.gameId).toBeDefined();
   });
 });
-describe("An other feature is dropping a disc in a column",()=>{
-it("When a player drops a disc in a column, the disc should fall to the bottom of the column",()=>{
-  const newGame = startNewGame();
-  const column = 0;
-  const updatedBoard = dropDisc(newGame.board, column);
-  expect(updatedBoard).toBeDefined();
-  expect(updatedBoard[5][0]).toEqual(1);
-});
+describe("An other feature is dropping a disc in a column", () => {
+  it("When a player drops a disc in a column, the disc should fall to the bottom of the column", () => {
+    const newGame = startNewGame();
+    const column = 0;
+    const updatedBoard = dropDisc(newGame.board, column);
+    expect(updatedBoard).toBeDefined();
+    expect(updatedBoard[5][0]).toEqual(1);
+  });
 });
